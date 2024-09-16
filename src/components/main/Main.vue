aa02053030288fcc8fcf5c7f16a3485a14f054b3
WangXing <19846811030@163.com>
2024-09-16 22:38:30 +0800

    modified:   src/components/main/Main.vue

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    
-  </div>
+    <div class="common-layout">
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script lang="ts" setup>
 
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
